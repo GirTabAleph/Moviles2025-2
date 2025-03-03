c9b96884af29423636ce14de47b4b86a54d56abd
GirTabAleph <nineeencycles@gmail.com>
2025-03-02 23:07:33 -0600

    Interfaz de la calculadora fue definida.

--- Proyecto260225/app/src/main/java/com/example/pryecto260225/MainActivity.java
+++ Proyecto260225/app/src/main/java/com/example/pryecto260225/MainActivity.java
@@ -1,10 +1,6 @@
-package com.example.proyecto240225;
+package com.example.pryecto260225;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,37 +10,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txvTitle;
-    Button btnContinuar;
-
-    ViewModel vm = new ViewModel();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        txvTitle = findViewById(R.id.txvTitle);
-        btnContinuar = findViewById(R.id.btnContinuar);
-
-        txvTitle.setText(R.string.actividad_uno);
-
-        btnContinuar.setOnClickListener(v -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                vm.navigate(this, SecondaryActivity.class);
-            }
-        });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
