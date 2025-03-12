089e6fdc1f9b2dda22c9a74c4975ec4fd78e8159
GirTabAleph <nineteencycles@gmail.com>
2025-03-11 23:00:49 -0600

    Clases para la lista de contactos.

---
+++ Contactos/app/src/main/java/com/example/contactos/Contact.java
@@ -0,0 +1,25 @@
+package com.example.contactos;
+
+public class Contact {
+
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone){
+
+    }
+
+    //Faltan los setters y getters.
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPhone(){
+        return this.phone;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+}
---
+++ Contactos/app/src/main/java/com/example/contactos/ContactAdapter.java
@@ -0,0 +1,64 @@
+package com.example.contactos;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder>{
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView txvName, txvPhone;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.txvName = itemView.findViewById(R.id.txvName);
+            this.txvPhone = itemView.findViewById(R.id.txvNumber);
+        }
+
+        public void setName(String txvName){
+            this.txvName.setText(txvName);
+        }
+
+        public void setPhone(String txvPhone){
+            this.txvPhone.setText(txvPhone);
+        }
+
+    }
+
+
+    List<Contact> contactList;
+
+    public ContactAdapter(List<Contact> contactList){
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_contact, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Contact contacto = contactList.get(position);
+
+        holder.setName(contacto.getName());
+        holder.setPhone(contacto.getPhone());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.contactList.size();
+    }
+}
