f83fed636f2ed53174c1c6534dd2cb49a9a5631a
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2025-05-19 16:02:45 -0600

    Borrar archivos no sincronizados

--- Proyecto260225/app/src/main/java/com/example/pryecto260225/CalculadoraOnResult.java
+++
@@ -1,9 +0,0 @@
-package com.example.pryecto260225;
-
-import java.math.BigDecimal;
-
-public interface CalculadoraOnResult {
-
-    void onResult(BigDecimal x, BigDecimal y, Operacion operacion);
-
-}
