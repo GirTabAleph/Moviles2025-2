b41c500a06024f3f0914ee746e8183c1f75f739d
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2025-05-19 15:59:03 -0600

    Delete baseDatos directory
    Borrar archivos no sincronizados

--- baseDatos/app/src/main/java/com/example/basedatos/AppDatabase.java
+++
@@ -1,8 +0,0 @@
-package com.example.basedatos;
-
-import androidx.room.*;
-
-@Database(entities = {User.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract UserDao userDao();
-}
